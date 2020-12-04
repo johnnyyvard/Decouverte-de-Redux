bdb11762091a689bd190fed8e0259f6c34c8a2cc
Johnny <johnnyyvard@me.com>
2020-12-04 17:31:49 +0100

    Configuration du store

--- src/index.js
+++ src/index.js
@@ -3,5 +3,13 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './components/App';
 import './css/ToDo.css';
+import { Provider } from 'react-redux'
+import configureStore from './store/store'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const store = configureStore();
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('root'))
